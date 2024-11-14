f750878ac6c6b781d69d8090ba80d4487ad78d95
Teerapat Kongchu <pkbboy2145@gmail.com>
2024-11-14 17:15:59 +0700

    feat/add firebase notification for android

--- android/app/src/main/java/com/fcmproject/MainActivity.kt
+++ android/app/src/main/java/com/fcmproject/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.fcmproject
+package com.example.tkmessage
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/fcmproject/MainApplication.kt
+++ android/app/src/main/java/com/fcmproject/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.fcmproject
+package com.example.tkmessage
 
 import android.app.Application
 import com.facebook.react.PackageList
